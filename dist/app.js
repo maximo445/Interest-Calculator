6965d22661f068fb754ebb664be376ae4f404f64
Maximo Brito <britofilammaker@gmail.com>
2022-01-04 18:41:57 -0500

    Final Logical Commit

--- dist/app.js
+++ dist/app.js
@@ -16,22 +16,37 @@ const bottom = document.querySelector('#bottom');
 const resultsContainer = document.querySelector('#results-container');
 const loadingContainer = document.querySelector('#loading-container');
 
+//warning variable 
+const warning = document.querySelector('#warning');
+
 //logic
 calculateBtn.addEventListener('click', calculate);
 
 function calculate () {
-    const interestAmountR = (parseFloat(loanAmount.value) * (parseFloat(interest.value)/100) * parseFloat(yearsToRepay.value));
-    const totalPaymentR = parseFloat(loanAmount.value) + interestAmountR;
-    const monthlyPaymentR = totalPaymentR / (parseFloat(yearsToRepay.value) * 12);
+    
+    resultsContainer.style.display = 'none';
 
-    monthlyPayment.innerHTML = `${monthlyPaymentR}`;
-    totalPayment.innerHTML = `${totalPaymentR}`;
-    totalInterest.innerHTML = `${interestAmountR}`;
+    if (interest.value === "" || yearsToRepay.value === "" || loanAmount.value === "") {
+        bottom.style.display = 'inline-block';
+        loadingContainer.style.display = 'flex';
+        setTimeout(displayWarning, 3200);
+    } else {
+        const interestAmountR = (parseFloat(loanAmount.value) * (parseFloat(interest.value)/100) * parseFloat(yearsToRepay.value));
+        const totalPaymentR = parseFloat(loanAmount.value) + interestAmountR;
+        const monthlyPaymentR = totalPaymentR / (parseFloat(yearsToRepay.value) * 12);
+    
+        console.log(typeof yearsToRepay.value);
+    
+        monthlyPayment.innerHTML = `${monthlyPaymentR}`;
+        totalPayment.innerHTML = `${totalPaymentR}`;
+        totalInterest.innerHTML = `${interestAmountR}`;
+        
+        bottom.style.display = 'inline-block';
+        loadingContainer.style.display = 'flex';
+    
+        setTimeout(displayResults, 3200);
+    }
 
-    bottom.style.display = 'inline-block';
-    loadingContainer.style.display = 'flex';
-
-    setTimeout(displayResults, 3200);
 }
 
 //delay and results logic
@@ -39,4 +54,13 @@ function calculate () {
 function displayResults () {
     resultsContainer.style.display = 'flex';
     loadingContainer.style.display = 'none';
+}
+
+function displayWarning () {
+    warning.style.display = 'inline-block'
+    loadingContainer.style.display = 'none';
+    bottom.style.display = 'none';
+    setTimeout(function () {
+        warning.style.display = 'none';
+    }, 3200);
 }
